52acfa1174caee31fcfddc4c40fa9ed024f7bf60
JFHer-live <changduochuan@gmail.com>
2025-04-20 19:02:31 -0600

    Update webhook to reply messages

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const axios = require("axios");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Webhook server is running!");
+});
+
+app.post("/", async (req, res) => {
+  const message = req.body;
+  console.log("收到訊息：", message);
+
+  const to = message?.data?.from;
+  const text = message?.data?.body;
+
+  if (to && text) {
+    await axios.post("https://api.ultramsg.com/你的_INSTANCE_ID/messages/chat", {
+      token: "你的_TOKEN",
+      to,
+      body: `你剛剛說了：${text}`,
+    });
+  }
+
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
